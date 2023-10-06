e2df686cf0ab59c41f40760c7e81e90fb24f88d1
tineojf <92743762+tineodev@users.noreply.github.com>
2023-10-06 09:04:33 -0500

    Add unit & dec & cent & main method

--- src/Conversor.java
+++ src/Conversor.java
@@ -16,7 +16,15 @@ public class Conversor {
 
     private String texto = "";
 
-    public void main() {
+    public String main(String numero) {
+        int digit = Integer.parseInt(numero);
+
+        if (digit <= 999) {
+            udc(digit);
+            return this.texto;
+        } else {
+            return "Numero fuera de rango";
+        }
     }
 
     // Metodos para valores hasta 999
@@ -34,11 +42,46 @@ public class Conversor {
         int dec = Integer.parseInt(decena);
         int uni = Integer.parseInt(unidad);
 
-        if (dec == 0) {
-            texto += valores29[uni];
+        if (uni == 0) {
+            texto += valoresDecena[dec];
         } else {
-            texto += valoresDecena[dec] + valores29[uni];
+            texto += valoresDecena[dec] + " y " + valores29[uni];
         }
+    }
 
+    public void valores999(int digit) {
+        //Cast & divide
+        String numero = String.valueOf(digit);
+        String centena = numero.substring(0, 1);
+        String decena = numero.substring(1, 2);
+        String unidad = numero.substring(2, 3);
+
+        //Cast a int
+        int cen = Integer.parseInt(centena);
+        int dec = Integer.parseInt(decena);
+        int uni = Integer.parseInt(unidad);
+
+        if (dec == 0 & uni == 0) {
+            if (cen == 1) {
+                texto += "cien";
+            } else {
+                texto += valoresCentena[cen];
+            }
+        } else {
+            texto += valoresCentena[cen] + " ";
+            udc(dec * 10 + uni);
+        }
+    }
+
+    public void udc(int digit) {
+        if (digit <= 29) {
+            valores29(digit);
+        } else if (digit <= 99) {
+            valores99(digit);
+        } else if (digit <= 999) {
+            valores999(digit);
+        } else {
+            System.out.println("Numero fuera de rango");
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         System.out.println("-------------------------");
         System.out.println("Conversor Digitos a Letras");
         System.out.println("-------------------------");
+
+        for (int i = 1; i < 1001; i++) {
+            Conversor c = new Conversor();
+            String numero = String.valueOf(i);
+            System.out.println(c.main(numero));
+        }
     }
 }
